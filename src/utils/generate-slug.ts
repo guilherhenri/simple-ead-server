56ec180ec445eba2418f692a0d8feb3b4e333bb9
guilherhenri <guilherhenri12@gmail.com>
2023-09-03 11:20:09 -0300

    Created generateSlug function

---
+++ src/utils/generate-slug.ts
@@ -0,0 +1,13 @@
+export function generateSlug(text: string) {
+  const slug = text
+    .normalize('NFKD')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/_/g, '-')
+    .replace(/--+/g, '-')
+    .replace(/-$/g, '')
+
+  return slug
+}
