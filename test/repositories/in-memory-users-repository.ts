93f9a219f624c8d7a852c85805ba5ca432f8f10d
guilherhenri <guilherhenri12@gmail.com>
2023-09-03 08:01:57 -0300

    Created in-memory-users-repository

---
+++ test/repositories/in-memory-users-repository.ts
@@ -0,0 +1,24 @@
+import { Prisma, User } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+import { UsersRepository } from '@/repositories/users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      cpf: data.cpf ?? null,
+      role: data.role ?? 'student',
+      created_at: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+}
