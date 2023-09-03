6ec930a5af5b5a86f15f58c23458e3759fc793da
guilherhenri <guilherhenri12@gmail.com>
2023-09-03 08:16:20 -0300

    Add findByEmail method

--- test/repositories/in-memory-users-repository.ts
+++ test/repositories/in-memory-users-repository.ts
@@ -6,6 +6,16 @@ import { UsersRepository } from '@/repositories/users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public users: User[] = []
 
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = {
       id: randomUUID(),
