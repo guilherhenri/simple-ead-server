012aa3044ef2e563bd975e56e257e4aa33c87df3
guilherhenri <guilherhenri12@gmail.com>
2023-09-04 09:26:44 -0300

    Added findById method

--- src/repositories/courses-repository.ts
+++ src/repositories/courses-repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, Course } from '@prisma/client'
 
 export interface CoursesRepository {
+  findById(id: string): Promise<Course | null>
   findByTitle(title: string): Promise<Course | null>
   findBySlug(slug: string): Promise<Course | null>
   create(data: Prisma.CourseUncheckedCreateInput): Promise<Course>
