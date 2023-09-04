1790852134a5085165144c88c2938fa5a1e249a5
guilherhenri <guilherhenri12@gmail.com>
2023-09-04 11:14:39 -0300

    Created class-modules-repository.ts

--- src/repositories/class-modules-repository.ts
+++ src/repositories/class-modules-repository.ts
@@ -1,6 +1,10 @@
 import { ClassModule, Prisma } from '@prisma/client'
 
 export interface ClassModulesRepository {
-  findByTitle(title: string): Promise<ClassModule | null>
+  findById(id: string): Promise<ClassModule | null>
+  findByTitleAndCourseId(
+    title: string,
+    courseId: string,
+  ): Promise<ClassModule | null>
   create(data: Prisma.ClassModuleUncheckedCreateInput): Promise<ClassModule>
 }
