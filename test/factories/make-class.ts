af48f9e23da2ed5452da6a1d37eff61edecd7746
guilherhenri <guilherhenri12@gmail.com>
2023-09-05 10:29:01 -0300

    Created factorie makeClass

---
+++ test/factories/make-class.ts
@@ -0,0 +1,20 @@
+import { ClassesRepository } from '@/repositories/classes-repository'
+import { generateSlug } from '@/utils/generate-slug'
+import { Class, Prisma } from '@prisma/client'
+
+export async function makeClass(
+  override: Partial<Prisma.ClassUncheckedCreateInput> = {},
+  classesRepository: ClassesRepository,
+  classModuleId: string,
+): Promise<Class> {
+  const lesson = await classesRepository.create({
+    title: 'module example',
+    description: 'module description',
+    class_module_id: classModuleId,
+    slug: generateSlug(override?.title ?? 'module-example'),
+    video_embed: '',
+    ...override,
+  })
+
+  return lesson
+}
