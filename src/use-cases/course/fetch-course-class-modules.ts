4893165153338a7386a8f4e8174d652ad0657b51
guilherhenri <guilherhenri12@gmail.com>
2023-09-07 09:06:17 -0300

    Created fech-course-class-modules use-case

---
+++ src/use-cases/course/fetch-course-class-modules.ts
@@ -0,0 +1,40 @@
+import { Either, left, right } from '@/core/either'
+import { ClassModulesRepository } from '@/repositories/class-modules-repository'
+import { CoursesRepository } from '@/repositories/courses-repository'
+import { ClassModule } from '@prisma/client'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+
+interface FetchCourseClassModulesUseCaseRequest {
+  courseId: string
+}
+
+type FetchCourseClassModulesUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    classModules: ClassModule[]
+  }
+>
+
+export class FetchCourseClassModulesUseCase {
+  constructor(
+    private classModulesRepository: ClassModulesRepository,
+    private coursesRepository: CoursesRepository,
+  ) {}
+
+  async execute({
+    courseId,
+  }: FetchCourseClassModulesUseCaseRequest): Promise<FetchCourseClassModulesUseCaseResponse> {
+    const courseExists = await this.coursesRepository.findById(courseId)
+
+    if (!courseExists) {
+      return left(new ResourceNotFoundError())
+    }
+
+    const classModules =
+      await this.classModulesRepository.findByCourseId(courseId)
+
+    return right({
+      classModules,
+    })
+  }
+}
